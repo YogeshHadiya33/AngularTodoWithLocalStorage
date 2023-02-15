480a92aae95f9d8337cd7e969f3d95a079fa72e1
YogeshHadiya <hadiyayk33@gmail.com>
2023-02-16 00:56:28 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'todo';
+  list: any = []
+  task: string = "";
+   
+
+
+  ngOnInit(): void {
+    this.GetAll();
+  }
+
+  Add() {
+    let obj = {
+      TaskName: this.task,
+      IsComplete: false
+    };
+    this.list.push(obj);
+    this.Save();
+    this.task = '';
+  }
+
+  MarkComplete(index: number, currentValue: boolean) {
+    if (this.list.length > index) {
+      let obj = this.list[index];
+      if (obj != null && typeof obj != "undefined") {
+        obj.IsComplete = !currentValue;
+      }
+      this.list[index] = obj;
+      this.Save();
+    }
+  }
+
+  Delete(index: number) {
+    if (this.list.length > index) {
+      this.list.splice(index, 1);
+      this.Save();
+    }
+  }
+
+
+  DeleteAll() {
+    this.list = [];
+    this.Save();
+  }
+
+  Save() {
+    localStorage.setItem("todo", JSON.stringify(this.list));
+  }
+
+  GetAll() {
+    let value = localStorage.getItem("todo");
+    if (value != '' && value != null && typeof value != "undefined") {
+      this.list = JSON.parse(value!);
+    }
+  }
 }
