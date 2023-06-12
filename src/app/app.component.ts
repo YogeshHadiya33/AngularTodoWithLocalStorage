7348eb298fecb9a754f6604e7428a2732ad9629d
Yogeshkumar Hadiya <yogesh.hadiya@sevinatech.com>
2023-06-12 18:52:07 +0530

    search operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { TaskModel } from './models/task.model';
 
 @Component({
   selector: 'app-root',
@@ -7,54 +9,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  list: any = []
+  list: TaskModel[] = []
   task: string = "";
+  searchText: string = "";
 
   ngOnInit(): void {
-    this.GetAll();
+    this.list = this.GetAll();
   }
 
   Add() {
-    let obj = {
-      TaskName: this.task,
-      IsComplete: false
-    };
-    this.list.push(obj);
-    this.Save();
-    this.task = '';
+    if (this.task != null && this.task != "") {
+      let obj: TaskModel = {
+        TaskId: new Date().getTime(),
+        TaskName: this.task,
+        IsComplete: false
+      };
+      this.list.push(obj);
+      this.Save(this.list);
+      this.task = '';
+    }
   }
 
-  ChangeStatus(index: number, currentValue: boolean) {
-    if (this.list.length > index) {
-      let obj = this.list[index];
+  ChangeStatus(taskId: number, currentValue: boolean) {
+    debugger;
+    let newList: TaskModel[] = this.GetAll();
+    let index = newList.findIndex(x => x.TaskId == taskId);
+    if (index != null && typeof index == "number" && newList.length > index) {
+      let obj = newList[index];
       if (obj != null && typeof obj != "undefined") {
         obj.IsComplete = !currentValue;
-        this.list[index] = obj;
-        this.Save();
+        newList[index] = obj;
+        this.Save(newList);
+        this.list = this.Search();
       }
     }
   }
 
-  Delete(index: number) {
-    if (this.list.length > index) {
-      this.list.splice(index, 1);
-      this.Save();
+  Delete(taskId: number) {
+    let newList: TaskModel[] = this.GetAll();
+    let index = newList.findIndex(x => x.TaskId == taskId);
+    if (index != null && typeof index == "number" && newList.length > index) {
+      newList.splice(index, 1);
+      this.Save(newList);
+      this.list = this.Search();
     }
   }
 
   DeleteAll() {
     this.list = [];
-    this.Save();
+    this.Save(this.list);
   }
 
-  Save() {
-    localStorage.setItem("todo", JSON.stringify(this.list));
+  Save(list: TaskModel[]) {
+    localStorage.setItem("todo", JSON.stringify(list));
   }
 
   GetAll() {
     let value = localStorage.getItem("todo");
     if (value != '' && value != null && typeof value != "undefined") {
-      this.list = JSON.parse(value!);
+      return JSON.parse(value!);
+    }
+    return [];
+  }
+
+  SearchTask() {
+    if (this.searchText != null && this.searchText != "" && typeof this.searchText != "undefined") {
+      this.list = this.Search();
     }
   }
+
+  Search() {
+    let newList = this.GetAll();
+    return newList.filter((x: TaskModel) => {
+      return x.TaskName.toLocaleLowerCase().includes(this.searchText.toLocaleLowerCase());
+    });
+  }
+
+  Clear() {
+    this.searchText = "";
+    this.list = this.Search();
+  }
+
 }
---
+++ src/app/models/task.model.ts
@@ -0,0 +1,6 @@
+
+export interface TaskModel{
+    TaskId:number,
+    TaskName:string,
+    IsComplete:boolean,
+}
